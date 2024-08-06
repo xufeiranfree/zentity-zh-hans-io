0e0276d3dcf7cd4b7597f88b438ce0b5099e801e
xufeiranfree <xu.feiran@qq.com>
2024-08-06 11:49:27 +0000

    change base

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -3,7 +3,7 @@ import { defineUserConfig } from 'vuepress/cli'
 import { viteBundler } from '@vuepress/bundler-vite'
 
 export default defineUserConfig({
-  base: '/zentity-zh-hans-io/',
+  base: '/',
   lang: 'zh-CN',
 
   title: 'zentity',
